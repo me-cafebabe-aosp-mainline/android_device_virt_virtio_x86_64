265f4a56d6d36fc934589980f5f9c1e02abba0ba
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-18 22:28:43 +0800

    virtio_x86_64: Support inline kernel building
    Change-Id: I316f98a6dbe99a12acc92bc7354e39a98a01d9de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,11 @@ TARGET_2ND_ARCH_VARIANT := x86_64
 BOARD_KERNEL_CMDLINE += \
     8250.nr_uarts=1 \
     console=ttyS0
+
+BOARD_KERNEL_IMAGE_NAME := bzImage
+TARGET_KERNEL_ARCH := x86
+
+ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
+BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/config/modules.load.ramdisk))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+endif
--- device.mk
+++ device.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel
-TARGET_KERNEL_ARCH := x86_64
-TARGET_KERNEL_MODULES_ARCH := x86-64
-TARGET_KERNEL_USE ?= 6.6
+# Kernel (Prebuilt)
+TARGET_PREBUILT_KERNEL_ARCH := x86_64
+TARGET_PREBUILT_KERNEL_MODULES_ARCH := x86-64
+TARGET_PREBUILT_KERNEL_USE ?= 6.6
 
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
