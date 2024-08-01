047a09a2f507e8ea79f55270308dfa4041259ee0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-01 11:18:19 +0800

    CipherOS: virtio_x86_64: Add cipher product makefile
    Change-Id: I79cfc4bd653f2b52f1389da86e6bf0133376a7ec

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_virtio_x86_64.mk \
+    $(LOCAL_DIR)/cipher_virtio_x86_64.mk \
     $(LOCAL_DIR)/lineage_virtio_x86_64.mk
 
 $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_virtio_x86_64-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += cipher_virtio_x86_64-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_virtio_x86_64-$(build_type)))
