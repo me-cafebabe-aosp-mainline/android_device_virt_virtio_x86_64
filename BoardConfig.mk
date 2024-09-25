ac47d720e1e220ca3c569a3a5d56be8e23c24b24
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-25 15:43:28 +0800

    [DNM] virtio_x86_64: Kernel configuration for 4.4 ~ 5.4
    Change-Id: I57d7e1371c2e928e3cdaba8e0356f5ee3ba56a67

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,3 +28,21 @@ BOARD_KERNEL_CMDLINE += \
 
 BOARD_KERNEL_IMAGE_NAME := bzImage
 TARGET_KERNEL_ARCH := x86
+
+# Kernel (Legacy)
+KERNEL_VERSION := $(shell grep -s "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_PATCHLEVEL := $(shell grep -s "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+
+ifeq ($(KERNEL_VERSION),4)
+    ifeq ($(KERNEL_PATCHLEVEL),19)
+        TARGET_KERNEL_CONFIG := gki_defconfig
+    else
+        TARGET_KERNEL_CONFIG := x86_64_cuttlefish_defconfig
+    endif
+else ifeq ($(KERNEL_VERSION),5)
+    TARGET_KERNEL_CONFIG := gki_defconfig
+endif
+
+TARGET_KERNEL_CONFIG += \
+    lineageos/virtio.config \
+    lineageos/feature/fbcon.config
--- device.mk
+++ device.mk
@@ -4,11 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel (Prebuilt)
-TARGET_PREBUILT_EMULATOR_KERNEL_USE ?= 6.6
-TARGET_PREBUILT_KERNEL_ARCH := x86_64
-TARGET_PREBUILT_KERNEL_MODULES_ARCH := x86-64
-
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
 
