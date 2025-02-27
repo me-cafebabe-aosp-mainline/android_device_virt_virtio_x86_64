9d936b10da238eb0f97fc7544df64bba207e3d13
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-28 06:46:18 +0800

    virtio_x86_64: Install GRUB i386-pc target
    Change-Id: I6f563b64f448b5ffd3c3b3858f7475ea72b2239b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,10 @@
 # Boot manager
 TARGET_BOOT_MANAGER ?= grub
 
+# GRUB
+TARGET_GRUB_ARCH := x86_64-efi
+TARGET_GRUB_2ND_ARCH := i386-pc
+
 # Inherit from common
 include device/virt/virtio-common/BoardConfigCommon.mk
 
@@ -28,9 +32,6 @@ BOARD_MESA3D_MESON_ARGS += -Dintel-clc=system
 BOARD_MESA3D_GALLIUM_DRIVERS += iris
 BOARD_MESA3D_VULKAN_DRIVERS += intel intel_hasvk
 endif
-
-# GRUB
-TARGET_GRUB_ARCH := x86_64-efi
 
 # Kernel
 BOARD_KERNEL_CMDLINE += \
